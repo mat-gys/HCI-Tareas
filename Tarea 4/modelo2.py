92bde6115a67c4902650809faa05067cdd933ff6
Denys Aldair Casiano Inga <77028527+denysc11@users.noreply.github.com>
2022-07-23 22:22:44 -0300

    Update modelo2.py

--- Tarea 4/modelo2.py
+++ Tarea 4/modelo2.py
@@ -4,7 +4,7 @@ Name : modelo2
 Group : 
 With QGIS : 32209
 """
-
+# Se importan los paquetes de QGIS necesarios 
 from qgis.core import QgsProcessing
 from qgis.core import QgsProcessingAlgorithm
 from qgis.core import QgsProcessingMultiStepFeedback
@@ -12,12 +12,12 @@ from qgis.core import QgsProcessingParameterRasterDestination
 from qgis.core import QgsCoordinateReferenceSystem
 import processing
 
-
+# Se define una clase para el modelo1:
 class Modelo2(QgsProcessingAlgorithm):
-
+    # Creamos función que crea un destino para los outputs creados por los algoritmos, dentro de esta apareceran cada una de las ramificaciones de lo programado en QGIS
     def initAlgorithm(self, config=None):
         self.addParameter(QgsProcessingParameterRasterDestination('Suitout', 'suitout', createByDefault=True, defaultValue=None))
-
+    # Creamos función principal
     def processAlgorithm(self, parameters, context, model_feedback):
         # Use a multi-step feedback, so that individual child algorithm progress reports are adjusted for the
         # overall progress through the model
@@ -25,7 +25,10 @@ class Modelo2(QgsProcessingAlgorithm):
         results = {}
         outputs = {}
 
+        # #######################################
         # Warp (reproject)
+        # #######################################           
+        # Procedemos a proyectar el raster hdr.adf que se guarde en un archivo tipo diccionario suitout.
         alg_params = {
             'DATA_TYPE': 0,  # Use Input Layer Data Type
             'EXTRA': '',
@@ -41,18 +44,22 @@ class Modelo2(QgsProcessingAlgorithm):
             'TARGET_RESOLUTION': None,
             'OUTPUT': parameters['Suitout']
         }
+        # Procesamos el INPUT a traves del método 'gdal:warpreproject', esto se almacena en un diccionario outputs con la key "Suitout"
         outputs['WarpReproject'] = processing.run('gdal:warpreproject', alg_params, context=context, feedback=feedback, is_child_algorithm=True)
         results['Suitout'] = outputs['WarpReproject']['OUTPUT']
-
+        
+        # Le digo en que paso del algoritmo estoy para dividirlo en distintos child steps, si algo salió mal, devuelve nada
         feedback.setCurrentStep(1)
         if feedback.isCanceled():
             return {}
 
         # Extract projection
+        # Esto también nos permitió crear el archivo en formato prj. 
         alg_params = {
             'INPUT': outputs['WarpReproject']['OUTPUT'],
             'PRJ_FILE_CREATE': True
         }
+        # Procesamos el INPUT a traves del método 'gdal:extractprojection'.
         outputs['ExtractProjection'] = processing.run('gdal:extractprojection', alg_params, context=context, feedback=feedback, is_child_algorithm=True)
         return results
 
